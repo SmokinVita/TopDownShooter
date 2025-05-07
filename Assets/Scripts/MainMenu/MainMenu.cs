08a07c82431a98e3c36e4e389ae0fef437ff456d
tyler smallwood <tylersmall2012@gmail.com>
2025-05-07 15:55:30 +0900

    Working on main menu fade to play.

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -2,11 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Image _blackPanel;
+    [SerializeField] private float _fadeSpeed = 2f;
+    private float _fadeTime = 0f;
+    private bool _canFade = true;
+
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        //_canFade = true;
+        //SceneManager.LoadScene(1);
+        StartCoroutine(FadeInRoutine());
     }
+
+    //Grab panel UI and increase Alpha to fade to black before changing scene
+    private void Update()
+    {
+        if (_canFade == true)
+        {
+            _blackPanel.material.color = new Color(1, 1, 1, Mathf.SmoothStep(0, 1, _fadeSpeed + Time.time));
+        }
+    }
+
+    IEnumerator FadeInRoutine()
+    {
+        while (_blackPanel.color.a != 1)
+        {
+            _fadeTime += Time.time + _fadeSpeed;
+            _blackPanel.color = new Color(1, 1, 1, Mathf.Lerp(0, 1, _fadeTime));
+            return null;
+        }
+        return null;
+    }
+
 }
